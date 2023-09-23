206f2b087d827983bd791bc886666c6ba308eea7
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2023-09-23 16:55:45 +0300

    Commit and Push

---
+++ src/CA219/stdnt.java
@@ -0,0 +1,22 @@
+package CA219;
+
+public class stdnt {
+    int age;
+    String name ="ali";
+    boolean isScienceMajor;
+    char a;
+
+
+    public static void main(String[] args) {
+        stdnt st = new stdnt();
+        System.out.println(st.name);
+        System.out.println(st.age);
+        System.out.println(st.isScienceMajor);
+        System.out.println(st.a);
+        // variables
+        int x ;
+        String faculty ;
+        System.out.println(x);
+        System.out.println(faculty);
+    }
+}
