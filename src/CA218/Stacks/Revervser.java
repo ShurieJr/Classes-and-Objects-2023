6d52ccb6034c6d18b6ceb5278e26b9f937eab170
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2023-10-09 14:26:11 +0300

    Commit and Push

---
+++ src/CA218/Stacks/Revervser.java
@@ -0,0 +1,37 @@
+package CA218.Stacks;
+
+import java.util.Stack;
+
+public class Revervser {
+
+    public static void main(String[] args) {
+        String input = "SOMALIA MOGADISHU";
+        String result = reverse(input);
+        System.out.println("String reversed: " + result);
+
+    }
+
+    public static String reverseBuffer(String input){
+        Stack<Character> s1 = new Stack<>();
+        for(char letter: input.toCharArray() ){
+            s1.push(letter);
+        }
+        StringBuffer reversed = new StringBuffer();
+        while(!s1.isEmpty()){
+            reversed.append(s1.pop());
+        }
+        return reversed.toString();
+    }
+    public static String reverse(String input){
+    Stack<Character> stack = new Stack<>();
+    //1 . push elements
+        for(int i =0; i<input.length(); i++){
+            stack.push(input.charAt(i));
+        }
+        String reversed = "";
+        while( !stack.isEmpty()){
+            reversed += stack.pop();
+        }
+        return reversed;
+    }
+}
